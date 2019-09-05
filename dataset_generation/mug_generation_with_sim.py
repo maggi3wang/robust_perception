f2038ded11d4dc0066b15f425bb24b77adb794b7
Maggie Wang <maggi3wang@gmail.com>
2019-09-05 17:56:48 -0400

    getting htere

--- dataset_generation/mug_generation_with_sim.py
+++ dataset_generation/mug_generation_with_sim.py
@@ -10,7 +10,6 @@ import sys
 
 import pydrake
 from pydrake.common import FindResourceOrThrow
-from pydrake.geometry import FrameId
 from pydrake.common.eigen_geometry import Quaternion, AngleAxis, Isometry3
 from pydrake.geometry import (
     Box,
@@ -18,8 +17,6 @@ from pydrake.geometry import (
     SceneGraph,
     Sphere
 )
-from pydrake.geometry.render import CameraProperties, DepthCameraProperties
-
 from pydrake.math import (RollPitchYaw, RigidTransform)
 from pydrake.multibody.tree import (
     SpatialInertia,
@@ -41,18 +38,48 @@ from pydrake.solvers.ipopt import (IpoptSolver)
 from pydrake.solvers.nlopt import (NloptSolver)
 from pydrake.solvers.snopt import (SnoptSolver)
 from pydrake.systems.analysis import Simulator
-from pydrake.systems.framework import AbstractValue, DiagramBuilder
+from pydrake.systems.framework import AbstractValue, DiagramBuilder, LeafSystem, PortDataType
 from pydrake.systems.meshcat_visualizer import MeshcatVisualizer
 from pydrake.systems.rendering import PoseBundle
-import pydrake.systems.sensors as mut
+from pydrake.systems.sensors import RgbdSensor, Image, PixelType, PixelFormat
+from pydrake.geometry.render import DepthCameraProperties, MakeRenderEngineVtk, RenderEngineVtkParams
 
-# Available image / pixel types.
-pixel_types = [
-    mut.PixelType.kRgba8U,
-    mut.PixelType.kDepth16U,
-    mut.PixelType.kDepth32F,
-    mut.PixelType.kLabel16I,
-]
+import matplotlib.pyplot as plt
+
+
+class RgbAndLabelImageVisualizer(LeafSystem):
+    def __init__(self,
+                 draw_timestep=0.033333):
+        LeafSystem.__init__(self)
+        self.set_name('image viz')
+        self.timestep = draw_timestep
+        self._DeclarePeriodicPublish(draw_timestep, 0.0)
+        
+        self.rgb_image_input_port = \
+            self._DeclareAbstractInputPort("rgb_image_input_port",
+                                   AbstractValue.Make(Image[PixelType.kRgba8U](640, 480, 3)))
+        self.label_image_input_port = \
+            self._DeclareAbstractInputPort("label_image_input_port",
+                                   AbstractValue.Make(Image[PixelType.kLabel16I](640, 480, 1)))
+        # self.fig = plt.figure()
+        # self.ax = plt.gca()
+        plt.draw()
+
+    def _DoPublish(self, context, event):
+        rgb_image = self.EvalAbstractInput(context, 0).get_value()
+        label_image = self.EvalAbstractInput(context, 1).get_value()
+        color_array = rgb_image.data
+        print('color_array: ', color_array)
+        label_array = label_image.data
+        print('label_array: ', label_array)
+
+        for x in np.nditer(label_array):
+            if not (x == 1):
+                print(x)
+        # print(rgb_image)
+        # print(label_image)
+        plt.pause(0.1)
+
 
 def main():
     np.random.seed(42)
@@ -60,16 +87,15 @@ def main():
     for scene_iter in range(1):
         try:
             builder = DiagramBuilder()
-
-            # Create a multibody plant and scene graph
             mbp, scene_graph = AddMultibodyPlantSceneGraph(
                 builder, MultibodyPlant(time_step=0.001))
-
+            renderer_params = RenderEngineVtkParams()
+            scene_graph.AddRenderer("renderer", MakeRenderEngineVtk(renderer_params))
             # Add ground
             world_body = mbp.world_body()
-            ground_shape = Box(2.0, 2.0, 2.0)
+            ground_shape = Box(2., 2., 2.)
             ground_body = mbp.AddRigidBody("ground", SpatialInertia(
-                mass=10.0, p_PScm_E=np.array([0.0, 0.0, 0.0]),
+                mass=10.0, p_PScm_E=np.array([0., 0., 0.]),
                 G_SP_E=UnitInertia(1.0, 1.0, 1.0)))
             mbp.WeldFrames(world_body.body_frame(), ground_body.body_frame(),
                            RigidTransform(Isometry3(rotation=np.eye(3), translation=[0, 0, -1])))
@@ -80,11 +106,10 @@ def main():
                 ground_body, RigidTransform.Identity(), ground_shape, "ground_col",
                 CoulombFriction(0.9, 0.8))
 
-            # Parses SDF and URDF input files into a MultibodyPlant and (optionally) a SceneGraph.
             parser = Parser(mbp, scene_graph)
 
             candidate_model_files = [
-                "/Users/maggiewang/Workspace/RobotLocomotion/robust_perception/dataset_generation/mug_clean/mug.urdf",
+                "/Users/maggiewang/Workspace/RobotLocomotion/robust_perception/dataset_generation/mug_clean/mug.urdf"
             ]
 
             n_objects = np.random.randint(3, 7)
@@ -105,42 +130,32 @@ def main():
             mbp.AddForceElement(UniformGravityFieldElement())
             mbp.Finalize()
 
-            # visualizer = builder.AddSystem(MeshcatVisualizer(
-            #     scene_graph,
-            #     zmq_url="tcp://127.0.0.1:6000",
-            #     draw_period=0.001))
-            # builder.Connect(scene_graph.get_pose_bundle_output_port(),
-            #                 visualizer.get_input_port(0))
+            visualizer = builder.AddSystem(MeshcatVisualizer(
+                scene_graph,
+                zmq_url="tcp://127.0.0.1:6000",
+                draw_period=0.001))
+            builder.Connect(scene_graph.get_pose_bundle_output_port(),
+                            visualizer.get_input_port(0))
 
-            # diagram = builder.Build()
-
-            # Use HDTV size.
-            width = 1280
-            height = 720
-
-            color_properties = CameraProperties(
-                width=width, height=height, fov_y=np.pi/6,
-                renderer_name="renderer")
-            depth_properties = DepthCameraProperties(
-                width=width, height=height, fov_y=np.pi/6,
-                renderer_name="renderer", z_near=0.1, z_far=5.5)
-
-            # Put it at the origin.
-            X_WB = RigidTransform()
-            parent_id = FrameId.get_new_id()
-            camera_poses = mut.RgbdSensor.CameraPoses(
-                X_BC=RigidTransform(), X_BD=RigidTransform())
-            sensor = mut.RgbdSensor(parent_id=parent_id, X_PB=X_WB,
-                                    color_properties=color_properties,
-                                    depth_properties=depth_properties,
-                                    camera_poses=camera_poses,
-                                    show_window=True)
-
-            visualizer = builder.AddSystem(sensor)
-            # OutputPort src, InputPort dest
+            # Add camera
+            depth_camera_properties = DepthCameraProperties(
+                width=640, height=480, fov_y=np.pi/2, renderer_name="renderer", z_near=0.1, z_far=2.0)
+            parent_frame_id = scene_graph.world_frame_id()
+            # TODO: read the drake docs about RgbdSensor and figure out what to set this transform to
+            # to get the camera upright and facing towards the origin.
+            camera_tf = RigidTransform(p=[0, 0, 0.6], rpy=RollPitchYaw([0, np.pi, 0]))
+            camera = builder.AddSystem(RgbdSensor(parent_frame_id, camera_tf, depth_camera_properties, show_window=True))
+            camera.DeclarePeriodicPublish(0.1, 0.)
             builder.Connect(scene_graph.get_query_output_port(),
-                visualizer.query_object_input_port())
-            diagram = builder.Build()   
+                            camera.query_object_input_port())
+
+            camera_viz = builder.AddSystem(RgbAndLabelImageVisualizer(draw_timestep=0.1))
+            builder.Connect(camera.color_image_output_port(),
+                            camera_viz.get_input_port(0))
+            builder.Connect(camera.label_image_output_port(),
+                            camera_viz.get_input_port(1))
+
+            diagram = builder.Build()
 
             diagram_context = diagram.CreateDefaultContext()
             mbp_context = diagram.GetMutableSubsystemContext(
@@ -153,6 +168,8 @@ def main():
                 offset = k*7
                 q0[(offset):(offset+4)] = poses[k][0]
                 q0[(offset+4):(offset+7)] = poses[k][1]
+
+
 
             simulator = Simulator(diagram, diagram_context)
             simulator.set_target_realtime_rate(1.0)
@@ -179,11 +196,12 @@ def main():
 
             def vis_callback(x):
                 mbp.SetPositions(mbp_context, x)
-                query_output = scene_graph.get_query_output_port().Eval(sg_context)
+                pose_bundle = scene_graph.get_pose_bundle_output_port().Eval(sg_context)
                 context = visualizer.CreateDefaultContext()
-                # context.FixInputPort(0, AbstractValue.Make(query_output))
-                # print(query_output)
+                context.FixInputPort(0, AbstractValue.Make(pose_bundle))
+                #print(pose_bundle.get_pose(0))
                 visualizer.Publish(context)
+                #print("Here")
 
             prog.AddVisualizationCallback(vis_callback, q_dec)
             prog.AddQuadraticErrorCost(np.eye(q0.shape[0])*1.0, q0, q_dec)
@@ -195,6 +213,7 @@ def main():
 #            print "Initial guess: ", q0
             start_time = time.time()
             solver = SnoptSolver()
+            #solver = NloptSolver()
             sid = solver.solver_type()
             # SNOPT
             prog.SetSolverOption(sid, "Print file", "test.snopt")
@@ -202,72 +221,42 @@ def main():
             prog.SetSolverOption(sid, "Major optimality tolerance", 1e-2)
             prog.SetSolverOption(sid, "Minor feasibility tolerance", 1e-3)
             prog.SetSolverOption(sid, "Scale option", 0)
+            #prog.SetSolverOption(sid, "Elastic weight", 1e1)
+            #prog.SetSolverOption(sid, "Elastic mode", "Yes")
+            # NLOPT
+            #prog.SetSolverOption(sid, "initial_step", 0.1)
+            #prog.SetSolverOption(sid, "xtol_rel", 1E-2)
+            #prog.SetSolverOption(sid, "xtol_abs", 1E-2)
+
+            #prog.SetSolverOption(sid, "Major step limit", 2)
 
             print("Solver opts: ", prog.GetSolverOptions(solver.solver_type()))
             print(type(prog))
             result = mp.Solve(prog)
             print("Solve info: ", result)
             print("Solved in %f seconds" % (time.time() - start_time))
-
+            #print(IpoptSolver().Solve(prog))
             print(result.get_solver_id().name())
             q0_proj = result.GetSolution(q_dec)
-
+#            print "Final: ", q0_proj
             mbp.SetPositions(mbp_context, q0_proj)
             q0_initial = q0_proj.copy()
             simulator.StepTo(10.0)
             q0_final = mbp.GetPositions(mbp_context).copy()
 
-            sensor_context = diagram.GetMutableSubsystemContext(sensor, diagram_context)
-            output = sensor.AllocateOutput()
-            sensor.CalcOutput(sensor_context, output)
-
-            # dut = mut.ImageToLcmImageArrayT(do_compress=False)
-            # # Declare ports.
-            # for pixel_type in pixel_types:
-            #     name = str(pixel_type)
-            #     dut.DeclareImageInputPort[pixel_type](name=name)
-            #     print('name', name)
-            # context = dut.CreateDefaultContext()
-            # for pixel_type in pixel_types:
-            #     name = str(pixel_type)
-            #     port = dut.GetInputPort(name)
-            #     # self._check_input(port)
-            #     image = mut.Image[pixel_type](width=1, height=1)
-            #     context.FixInputPort(port.get_index(), AbstractValue.Make(image))
-            # output = dut.AllocateOutput()
-            # dut.CalcOutput(context, output)
-
-            # sensor_context = sensor.CreateDefaultContext()
-            # output = sensor.AllocateOutput()
-            # sensor.CalcOutput(sensor_context, output)
-
-            # Get images from computed output.
-            # color_index = sensor.color_image_output_port().get_index()
-            # print('color_index: ', color_index)
-            # color_image = output.get_data(color_index).get_value()
-            # print('color_image: ', color_image)
-            # color_array = color_image.data
-            # print('color_array: ', color_array)
-
-            # hello = color_array
-            # print('hello', hello)
-
-            # depth_index = sensor.depth_image_output_port().get_index()
-            # depth_image = output.get_data(depth_index).get_value()
-            # depth_array = depth_image.data
-
-            # # Show camera info and images.
-            # print("Intrinsics:\n{}".format(sensor.depth_camera_info().intrinsic_matrix()))
-            # dpi = mpl.rcParams['figure.dpi']
-            # figsize = np.array([color_image.width(), color_image.height()*2]) / dpi
-            # plt.figure(1, figsize=figsize)
-            # plt.subplot(2, 1, 1)
-            # plt.imshow(color_array)
-            # # plt.subplot(2, 1, 2)
-            # # mpl does not like singleton dimensions for single-channel images.
-            # # plt.imshow(np.squeeze(depth_array))
-            # plt.show()
-
+            #output_dict = {"n_objects": len(poses)}
+            #for k in range(len(poses)):
+            #    offset = k*7
+            #    pose = q0[(offset):(offset+7)]
+            #    output_dict["obj_%04d" % k] = {
+            #        "class": classes[k],
+            #        "pose": pose.tolist()
+            #    }
+            #with open("tabletop_arrangements.yaml", "a") as file:
+            #    yaml.dump({"env_%d" % int(round(time.time() * 1000)):
+            #               output_dict},
+            #              file)
+#
             time.sleep(1.0)
 
         except Exception as e:
